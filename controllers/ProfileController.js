777f43f7e1838ac1d7360e27e5fc4065286da7d5
Alex Lavoie <alexflavoie@gmail.com>
2020-04-03 16:15:38 -0700

    Created the other controllers

---
+++ controllers/ProfileController.js
@@ -0,0 +1,13 @@
+let userModel = require('../models/UserModel');
+
+exports.editProfile = (req, res) => {
+
+};
+
+exports.getProfile = (req, res) => {
+
+};
+
+exports.createProfile = (req, res) => {
+
+};
