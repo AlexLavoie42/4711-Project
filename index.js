666ebb11f60e149d0fbc0eabec5a8b1ed18fd502
Daniel Chu <danieljanchu@outlook.com>
2020-04-02 19:16:43 -0700

    added expressHbs to index.js, modified main-layout

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 let express = require('express');
+let app = express();
+
+const expHbs = require('express-handlebars');
+
+app.use(express.static('public'));
+
+app.engine(
+    'hbs',
+    expHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.get('/', function (req, res) {
+    res.render('LandingView', {
+        pageTitle: 'Welcome to PetBase',
+        style: 'landingView.css'
+    });
+});
+
+app.listen(8000, () => console.log('Server ready'))
