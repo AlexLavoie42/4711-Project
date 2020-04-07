73ec9924f1013d3a42f1a711487fc2519dd4a04c
Berty <BertNocon2@gmail.com>
2020-04-06 21:12:24 -0700

    Added getPost, addPost

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,10 +1,18 @@
 let postModel = require('../models/PostModel');
 
 exports.getAllPosts = (req, res) => {
+    let Posts = postModel.getAll();
+    Posts.then( ([rows, fieldData]) => {
+        res.render('posts', { post: rows, postsCSS: true });
+    });
 
 };
 
 exports.getPost = (req, res) => {
+    let Posts = postModel.getPost(req.params['id']);
+    Posts.then( ([rows, fieldData]) => {
+        res.render('posts', { post: rows, postsCSS: true });
+    });
 
 };
 
@@ -17,7 +25,18 @@ exports.deletePost = (req, res) => {
 };
 
 exports.addPost = (req, res) => {
+    let p_name = req.body.name;
+    let p_about = req.body.about;
+    let p_imageURL = req.body.imageURL;
 
+    let pOject = {
+        name: p_name,
+        about: p_about,
+        imageURL: p_imageURL
+    }
+
+    peopleModel.add(pOject);
+    res.redirect(301, '/peoples');
 };
 
 exports.sendReply = (req, res) => {
