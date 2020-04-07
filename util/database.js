7c0422f275354a502471b771223fd06a4f95e01c
AlexLavoie <lavoiealex@hotmail.com>
2020-04-06 18:04:27 -0700

    Actually hooked up db

--- util/database.js
+++ util/database.js
@@ -1,4 +1,4 @@
 const mysql = require("mysql2");
 
-pool = mysql.createPool({host: 'remotemysql.com:3306', user: 'BkwEmFKpeJ', database: 'BkwEmFKpeJ', password: 'iKzvTOVXs7'});
+pool = mysql.createPool({host: 'remotemysql.com', user: 'BkwEmFKpeJ', database: 'BkwEmFKpeJ', password: 'iKzvTOVXs7'});
 module.exports = pool.promise();
