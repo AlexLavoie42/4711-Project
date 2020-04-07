21a7bca1ed9d128e15831445e05a1da78d3f0d14
AlexLavoie <lavoiealex@hotmail.com>
2020-04-07 13:53:44 -0700

    Almost got messages working

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,25 +1,48 @@
 let conversationModel = require('../models/ConversationModel');
 let messageModel = require('../models/MessageModel');
+let userModel = require('../models/UserModel');
 
 exports.getMessage = (req, res) => {
     let id = req.params['id'];
     let message = messageModel.getmessage(id);
     message.then((rows, err) => {
-        res.render("MessageView", {message: rows[0], style: "messageView.css"})
+        let convos = conversationModel.getconversation(id);
+        let messages = rows[0];
+        convos.then((rows, err) => {
+            convos = rows[0];
+            let user = userModel.getuser(convos[0]['id']);
+            user.then((rows, err) => {
+                user = rows[0];
+                for(c in convos){
+                    res.render("MessageView", {id: id, user: user, message: messages, style: "messageView.css"})
+                }
+            });
+        });
     })
 };
 
 exports.getAllMessages = (req, res) => {
     let conv = conversationModel.getuser(req.session.user.id);
     conv.then((rows, err) => {
-        res.render("MessageView", {messages: rows[0], style: "messageView.css"})
+        let convs = rows[0];
+        let data = userModel.getall();
+        data.then((rows, err) => {
+            let users = rows[0];
+            let messages = [];
+            for (c of convs) {
+                messages.push({...users[c['user1_id']], ...c})
+            }
+            res.render("MessageView", {messages: messages, style: "messageView.css"})
+        });
     })
 };
 
 exports.startConversation = (req, res) => {
     let user = req.session.user.id;
     let messaged = req.body.id;
-    conversationModel.add({subject: "", user1: user, user2: messaged})
+    let m_created = new Date();
+    m_created = m_created.toISOString().split('T')[0]+' ' + p_created.toTimeString().split(' ')[0];
+    conversationModel.add({subject: "", user1: user, user2: messaged});
     res.redirect('/messages')
 };
 
